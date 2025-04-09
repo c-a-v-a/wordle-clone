a28cc9f06206b795bd978ed0b452cef13b056013
c-a-v-a <cavaevilmail@gmail.com>
2025-04-10 00:16:19 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "CLI Wordle clone written in java";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [ jdk maven ];
+            shellHook = ''
+              alias check='./scripts/check'
+              alias format='./scripts/format'
+              alias package='./scripts/package'
+              alias report='./scripts/report'
+              alias runtests='./scripts/runtests'
+            '';
+          };
+        }
+      );
+}
