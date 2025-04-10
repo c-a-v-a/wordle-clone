7ea9fb46ea34989e0c7bf1ea2eb4480b6bb39886
Michal Brzezinski <michal.brzezinski.off@gmail.com>
2025-04-10 12:24:02 +0200

    wordle v1 MB

--- src/main/java/com/mbfc/app/App.java
+++ src/main/java/com/mbfc/app/App.java
@@ -3,6 +3,6 @@ package com.mbfc.app;
 /** Hello world. */
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello Wordle Clone!");
   }
 }
